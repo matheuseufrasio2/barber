9aed629f03ebaeafbd48f522b4f3e98c439c7f19
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2024-01-31 19:26:50 -0300

    feat: add initial header

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../_components/header'
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>home</h1>
+    </div>
+  )
+}
---
+++ src/app/_components/header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import { Card, CardContent } from './ui/card'
+import { Button } from './ui/button'
+
+import { MenuIcon } from 'lucide-react'
+
+export function Header() {
+  return (
+    <Card>
+      <CardContent className="flex items-center justify-between p-5">
+        <Image src="/logo.png" alt="Barber" height={22} width={120} />
+        <Button variant="outline" size="icon" className="h-8 w-8">
+          <MenuIcon size={18} />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
