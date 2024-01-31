fe4f519f0f2040ab723a67e345da4c46d3c75a5e
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2024-01-31 19:35:00 -0300

    feat: add welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,20 @@
+import { format } from 'date-fns'
 import { Header } from '../_components/header'
+import { ptBR } from 'date-fns/locale'
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <h1>home</h1>
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Matheus!</h2>
+        <p className="text-sm capitalize">
+          {format(new Date(), "EEEE',' dd' de 'MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   )
 }
