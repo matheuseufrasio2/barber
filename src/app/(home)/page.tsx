8cc7b52c0dc37205750eff16ce02b67f88517c28
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2024-01-31 20:02:36 -0300

    feat: add initial search

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,24 @@
 import { format } from 'date-fns'
 import { Header } from '../_components/header'
 import { ptBR } from 'date-fns/locale'
+import Search from './_components/search'
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <div className="p-5">
+      <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Matheus!</h2>
         <p className="text-sm capitalize">
           {format(new Date(), "EEEE',' dd' de 'MMMM", {
             locale: ptBR,
           })}
         </p>
+      </div>
+
+      <div className="mt-6 px-5">
+        <Search />
       </div>
     </div>
   )
